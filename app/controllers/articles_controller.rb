1b8e0387ffef58c771fb320203e382197e8667d8
Carlos Marques <cnmarques@gmail.com>
2023-05-26 10:52:21 +0100

    Create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,23 @@
 class ArticlesController < ApplicationController
 
     def show
-        #debugger
-        @article =  Article.find(params[:id])
+      #debugger
+      @article =  Article.find(params[:id])
     end
 
     def index
-        @articles = Article.all
+      @articles = Article.all
+    end
+
+    def new
+
+    end
+
+    def create
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      @article.save
+      redirect_to article#_path(@article)
+      #render plain: @article.inspect
     end
 
 end
