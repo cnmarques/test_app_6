6ceee2f214d7548ec62ad3ea5a2534a619f29c3f
Carlos Marques <cnmarques@gmail.com>
2023-05-26 11:11:43 +0100

    Show Validation errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,14 +10,18 @@ class ArticlesController < ApplicationController
     end
 
     def new
-
+      @article = Article.new
     end
 
     def create
       @article = Article.new(params.require(:article).permit(:title, :description))
-      @article.save
-      redirect_to article#_path(@article)
-      #render plain: @article.inspect
+      if @article.save
+        flash[:notice] = "Article created successfully!"
+        redirect_to @article#_path(@article)
+        #render plain: @article.inspect
+      else
+        render 'new'
+      end
     end
 
 end
