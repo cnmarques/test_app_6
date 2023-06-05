40629408bac1318f92081d923b503b749508ba23
Carlos Marques <cnmarques@gmail.com>
2023-06-05 17:00:24 +0100

    Refactoring DRY!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,49 +1,61 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-    def show
-      #debugger
-      @article =  Article.find(params[:id])
+  def show
+    #debugger
+    #@article =  Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    #@article =  Article.find(params[:id])
+  end
+
+
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+    flash[:notice] = "Article created successfully!"
+    redirect_to @article#_path(@article)
+    #render plain: @article.inspect
+    else
+    render 'new'
     end
+  end
 
-    def index
-      @articles = Article.all
-    end
+  def update
+    #@article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article updated successfully!"
+      redirect_to @article#_path(@article)
+      #render plain: @article.inspect
+    else
+      render 'edit'
+    end   
+  end
 
-    def new
-      @article = Article.new
-    end
+  def destroy
+    #@article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 
-    def edit
-        @article =  Article.find(params[:id])
-    end
+  private
 
+  def set_article
+    @article =  Article.find(params[:id])
+  end
 
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
-    def create
-      @article = Article.new(params.require(:article).permit(:title, :description))
-      if @article.save
-        flash[:notice] = "Article created successfully!"
-        redirect_to @article#_path(@article)
-        #render plain: @article.inspect
-      else
-        render 'new'
-      end
-    end
-
-    def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
-          flash[:notice] = "Article updated successfully!"
-          redirect_to @article#_path(@article)
-          #render plain: @article.inspect
-        else
-          render 'edit'
-        end   
-    end
-
-    def destroy
-        @article = Article.find(params[:id])
-        @article.destroy
-        redirect_to articles_path
-    end
 end
